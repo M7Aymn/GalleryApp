8cfad771caebab9cc4c2ef99e7a5af6d93f9090d
M7Aymn <modyhemead@gmail.com>
2024-11-18 20:07:12 +0200

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Mohamed Ayman on 18/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
